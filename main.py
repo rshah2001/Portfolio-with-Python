14c1aa42c84a4e70630ebc3b836bb7b024e5c594
Rishil <rishil1211@outlook.com>
2024-06-30 22:51:28 -0400

    Added Description on the new columns

--- main.py
+++ main.py
@@ -20,3 +20,8 @@ with col2:
     valuable asset to any team or organization.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me! 
+"""
+st.write(content2)
